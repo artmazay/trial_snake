9579f58405c3aa840c14d74c0a5b387f8a8b5061
artmazay <art.mazay@list.ru>
2016-03-26 16:08:52 +0200

    lesson2

---
+++ trial_snake/trial_snake/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 7, y1 = 11, x2 = 4, y2 = 5;
+            char symv = '*', sym = '"';
+            for (int i = 0; i <= 10; i++)
+            {
+                Draw(x1, y1--, sym);
+            }
+            Console.ReadKey();
+        }
+
+        private static void Draw(int x1, int y1, char symv)
+        {
+            Console.SetCursorPosition(x1, y1);
+            Console.WriteLine(symv);
+        }
+    }
+}
+
